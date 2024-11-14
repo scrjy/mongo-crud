fb2a2e6cd454c553ed6de7cc60ddb1a6932bb005
scrjy <sjyplus123@naver.com>
2024-11-14 17:24:53 +0900

    1st

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import TopicList from '@/components/TopicList'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">WebDev Topics</h1>
+      <p className="mb-4">MongoDB CRUD Example</p>
+      <TopicList />
+    </div>
+  )
+}
